83359e6f74c2cc49981a96531d20260dacbbc21b
Greg Lis <lisgrego@msu.edu>
2024-12-05 23:02:21 -0500

    More fixing of bad merge

--- RotoScope/resource.h
+++ RotoScope/resource.h
@@ -50,14 +50,14 @@
 #define ID_MOUSEMODE_GREG               32802
 #define ID_EDIT_MORPH                   32803
 #define ID_EDIT_GREENSCREEN             32805
-#define ID_MOUSEMODE_RECOLOR            32807
+#define ID_MOUSEMODE_RECOLOR            32808
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        132
-#define _APS_NEXT_COMMAND_VALUE         32806
+#define _APS_NEXT_COMMAND_VALUE         32807
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
